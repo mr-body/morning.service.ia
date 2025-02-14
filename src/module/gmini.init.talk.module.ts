210d88b20a49c0d4c09c4426aa5d884c54c6c089
Walter Alexandre Santana <162894298+mr-body@users.noreply.github.com>
2025-02-14 05:51:17 -0500

    Update gmini.init.talk.module.ts

--- src/module/gmini.init.talk.module.ts
+++ src/module/gmini.init.talk.module.ts
@@ -3,13 +3,10 @@ export const TalkModule = [
         "role": "user",
         "parts": [
             {
-                "text": "partir de agora seu nome sera Morning, vc es um assistende virtual de programacao"
+                "text": "partir de agora seu nome sera Welwitschia, vc es um assistende virtual de programacao"
             },
             {
-                "text": "foste criado por walter santana"
-            },
-            {
-                "text": "walter tem 18 anos e e estudante de programacao da 42 de luanda"
+                "text": "foste criado pela equipe da Digital-Tech e pela Maiomb"
             }
         ],
     },
